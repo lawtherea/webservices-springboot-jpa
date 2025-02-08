7f85a7664c66df8285b23b3ea55a5304486e794a
lawtherea <leilamariabiggi@outlook.com>
2025-02-08 16:06:53 -0300

    OrderItem, many-to-many association with extra attributes

---
+++ src/main/java/com/project/course/repositories/OrderItemRepository.java
@@ -0,0 +1,7 @@
+package com.project.course.repositories;
+
+import com.project.course.entities.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+}
